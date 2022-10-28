87aae05192fc08a7a79eab24e7f4260c3d465f83
Sebastian Verguecio <sebastian.verguecio@gmail.com>
2022-10-27 21:34:14 -0300

    Aistencia de Verguecio Sebastian

---
+++ AsistenciaVerguecioSebastian.py
@@ -0,0 +1,12 @@
+#crear un codigo de ejemplo que permita ingresar un numero entero y muestre su cuadrado y su cubo
+
+#ingreso de datos
+numero = int(input("Ingrese un numero entero: "))
+#proceso
+cuadrado = numero ** 2
+cubo = numero ** 3
+#salida
+print("El cuadrado de", numero, "es", cuadrado)
+print("El cubo de", numero, "es", cubo)
+
+# Path: AsistenciaVerguecioSebastian.py
