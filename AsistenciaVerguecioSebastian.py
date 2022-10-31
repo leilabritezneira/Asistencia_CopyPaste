ee57a28ee55ad978663cacd165666b07de2cb70f
Sebastian Verguecio <sebastian.verguecio@gmail.com>
2022-10-31 20:44:50 -0300

    Ejerccio Asistencia Octubre -- Cubo Python

--- AsistenciaVerguecioSebastian.py
+++ AsistenciaVerguecioSebastian.py
@@ -1,13 +1,20 @@
-#crear un codigo de ejemplo que permita ingresar un numero entero y muestre su cuadrado y su cubo
+class Cubo:
+    
+    # Ejercicio Cubo: Verguecio Sebastian
+    # Crear la clase Cubo con los atributos, alto, ancho, profundidad, con un metodo que se llame calcular_volumen que tendra la formula: volumen = ancho * altura * profundidad que el usuario ingrese los valores.
+    
+    def __init__(self, ancho, alto, profundidad):
+        self.ancho = ancho
+        self.alto = alto
+        self.profundidad = profundidad
 
-#ingreso de datos
-numero = int(input("Ingrese un numero entero: "))
-#proceso
-cuadrado = numero ** 2
-cubo = numero ** 3
-#salida
-print("El cuadrado de", numero, "es", cuadrado)
-print("El cubo de", numero, "es", cubo)
+    def calcular_volumen(self):
+        return self.ancho * self.alto * self.profundidad
 
-# Path: AsistenciaVerguecioSebastian.py
-# Otro comentario
+alto = int(input(f"Digite el numero para el alto del cubo: "))
+ancho = int(input(f"Digite el numero para el ancho del cubo: "))
+profundidad = int(input(f"Digite el numero para la profundidad del cubo: "))
+
+cubo1 = Cubo(alto, ancho, profundidad)
+
+print(f"El volumen del cubo es: {cubo1.calcular_volumen()}")
